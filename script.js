6854220d208ea42aa684ccd8fc8fc0e0fbac3ba3
William Lima <73370617+willwlima@users.noreply.github.com>
2021-12-30 15:30:24 -0300

    Atualização

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".typing", {
-  strings: ["", "Freelancer PJ", "", "Desenvolvedor Web", "", "Designer gráfico", "", "Marketing Digital"],
+  strings: ["", "Programador", "Freelancer PJ", "", "Desenvolvedor Web", "", "Designer gráfico", "", "Marketing Digital"],
   typeSpeed: 100,
   backSpeed: 60,
   loop: true
